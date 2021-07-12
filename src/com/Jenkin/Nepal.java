5a004e101839fb256e9f9c48385f7ef53747b1e7
anillama <33677877+anillama@users.noreply.github.com>
2021-07-12 16:45:10 -0600

    added all files

---
+++ src/com/Jenkin/Nepal.java
@@ -0,0 +1,10 @@
+package com.Jenkin;
+
+public class Nepal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello From Java to Jenkins");
+	}
+
+}
