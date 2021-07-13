97c2670c286b182421f574aba388aef549752335
anillama <33677877+anillama@users.noreply.github.com>
2021-07-13 08:50:41 -0600

    added few lines

--- src/com/Jenkin/Nepal.java
+++ src/com/Jenkin/Nepal.java
@@ -5,6 +5,8 @@ public class Nepal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello From Java to Jenkins");
+		System.out.println("Hello From Java");
+		System.out.println("Hello From Git");
 	}
 
 }
